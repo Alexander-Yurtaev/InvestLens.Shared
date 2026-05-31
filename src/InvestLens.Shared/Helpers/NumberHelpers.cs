4da728a98bddbc68af3ad17883553d49d3e36e51
alexander.yurtaev <alexander.yurtaev@gmail.com>
2026-05-31 14:28:34 +0300

    Remove Helpers, fix code and tests.

---
+++ src/InvestLens.Shared/Helpers/NumberHelpers.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace InvestLens.Shared.Helpers;
+
+public static class NumberHelpers
+{
+    public static string ConvertValueToString(decimal value)
+    {
+        return value.ToString(IsInt(value) ? "N0" : "F1", CultureInfo.InvariantCulture);
+    }
+
+    public static string ConvertPersentValueToString(decimal value)
+    {
+        return value.ToString("P2", CultureInfo.InvariantCulture);
+    }
+
+    public static object ConvertToNumber(string value)
+    {
+        var number = decimal.Parse(value, CultureInfo.InvariantCulture);
+        if (IsInt(number))
+        {
+            return (int)number;
+        }
+
+        return number;
+    }
+
+    public static bool IsInt(decimal value)
+    {
+        return Math.Abs(value - Math.Round(value)) == 0;
+    }
+}
